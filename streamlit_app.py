7d1eb3a99729d51ff41296fe1248332c2561c846
Pakshal Shah <49267670+PakshalShah30@users.noreply.github.com>
2023-03-06 10:21:09 -0600

    Update streamlit_app.py
    add 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Tony Stark v/s Captain America')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
