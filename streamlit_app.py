4d9841a8837d9bad9c90c5a739027c5668972a7c
Pakshal Shah <49267670+PakshalShah30@users.noreply.github.com>
2023-03-08 02:30:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 ## Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple'])
+fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
@@ -33,13 +33,22 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-# normalize json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output
-streamlit.dataframe(fruityvice_normalized)
 
+streamlit.header('Fruityvise Food Advice!')
 
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')  
+    if not fruit_choice:
+      streamlit.error('Please select a fruit!')
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 #import snowflake.connector
+
+except URLError as e:
+  streamlit.error();
+
 
 
 
